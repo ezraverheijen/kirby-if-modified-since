3e4a365600a88cd85fe8f64bd2b3e6059e15a370
Ezra Verheijen <everheijen@trimm.nl>
2017-01-12 15:28:21 +0100

    Removed redundant code

--- if-modified-since.php
+++ if-modified-since.php
@@ -2,24 +2,20 @@
 
 namespace EzraVerheijen\Plugins;
 
-require __DIR__ . DS . 'if-modified-since' . DS . 'if-modified-since.php';
-
 use C;
-use EzraVerheijen\Classes\IfModifiedSince as IMS;
-use Header;
-use Kirby\Component\Response;
 use Server;
 
-class IfModifiedSince extends Response {
+class IfModifiedSince extends \Kirby\Component\Response {
   
   public function make($response) {
     if(is_a($response, 'Page')) {
       if(!$response->isErrorPage() && !in_array($response->template(), (array)c::get('ifmodifiedsince.ignore'))) {
-        try {
-          new IMS($response->modified());
-        } catch(Exception $e) {
-          // I don't think this could normally happen,
-          // but if it would, there is not much we can do about it here...
+        $mtime = $response->modified();
+        if($mtime && ($ims = server::get('HTTP_IF_MODIFIED_SINCE')) && strtotime($ims) >= $mtime) {
+          http_response_code(304);
+          exit;
+        } else {
+          header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $mtime) . ' GMT');
         }
       }
     }
@@ -28,4 +24,4 @@ class IfModifiedSince extends Response {
   
 }
 
-$kirby->set('component', 'response', 'EzraVerheijen\Plugins\IfModifiedSince');
+$kirby->set('component', 'response', 'EzraVerheijen\\Plugins\\IfModifiedSince');
